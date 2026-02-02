0117e56f6979c2ec3377b5f5106a0f76ab8a3884
Fritz Venus M. Domingo <fritzvenusmanzanodomingo@gmail.com>
2026-02-02 22:13:15 +0800

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,3 @@
+year = int(input("Enter number of years: "))
+second = year * 365 * 24 * 60 * 60
+print(second)
---
+++ activity2.py
@@ -0,0 +1,9 @@
+x = int(input("Enter a number: "))
+y = int(input("Enter an another number: "))
+ 
+print(x > y)
+print(x >= y)
+print( x != y)
+print(x < y)
+print(x <= y)
+print(x == y)
